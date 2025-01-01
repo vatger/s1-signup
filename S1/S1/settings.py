1b9e51deba616326448b546b23ed0b0f0ec5a478
fsoest <felix@soest-net.de>
2025-01-01 21:34:35 +0100

    Static files

--- S1/S1/settings.py
+++ S1/S1/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = "/opt/s1/S1/static/"
-STATIC_URL = "/opt/s1/S1/static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
