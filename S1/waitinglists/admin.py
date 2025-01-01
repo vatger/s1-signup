0708f59518d153841cbca9eafa7b079faffa9c3d
fsoest <felix@soest-net.de>
2025-01-01 16:54:42 +0100

    New Session Admin interface

--- S1/waitinglists/admin.py
+++ S1/waitinglists/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
 
 from .models import Attendance, Module, Session, Signup, WaitingList
 
-# Register your models here.
+
+class SessionAdmin(admin.ModelAdmin):
+    # Other configurations like list_display, search_fields, etc.
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "mentor":
+            # Filter users who belong to the "Mentor" group
+            mentor_group = Group.objects.filter(name="Mentor").first()
+            if mentor_group:
+                kwargs["queryset"] = User.objects.filter(groups=mentor_group)
+            else:
+                kwargs["queryset"] = (
+                    User.objects.none()
+                )  # No group found, no users displayed
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
 
 admin.site.register(Attendance)
 admin.site.register(Module)
-admin.site.register(Session)
+admin.site.register(Session, SessionAdmin)
 admin.site.register(Signup)
 admin.site.register(WaitingList)
