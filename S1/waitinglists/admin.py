a4aa82b92d52b4b6705367323684f1c7d39ba64b
fsoest <felix@soest-net.de>
2025-01-01 17:15:18 +0100

    Admin interface

--- S1/waitinglists/admin.py
+++ S1/waitinglists/admin.py
@@ -6,27 +6,19 @@ from .models import Attendance, Module, Session, Signup, WaitingList
 
 
 class SessionAdmin(admin.ModelAdmin):
+    # Other configurations like list_display, search_fields, etc.
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "mentor":
             # Filter users who belong to the "Mentor" group
             mentor_group = Group.objects.filter(name="Mentor").first()
             if mentor_group:
                 kwargs["queryset"] = User.objects.filter(groups=mentor_group)
-                # Customize the display label in the dropdown
-                kwargs["queryset"] = kwargs["queryset"].order_by(
-                    "first_name", "last_name"
-                )
             else:
                 kwargs["queryset"] = (
                     User.objects.none()
                 )  # No group found, no users displayed
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-    # Optional: Customize how users are displayed in the dropdown
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        qs = qs.annotate(full_name=Concat("first_name", Value(" "), "last_name"))
-        return qs
 
 
 admin.site.register(Attendance)
