f5b07ad3a4abaa9342f5e065d0c6f6ed5e25db4c
fsoest <felix@soest-net.de>
2025-01-01 17:10:57 +0100

    Admin interface

--- S1/waitinglists/admin.py
+++ S1/waitinglists/admin.py
@@ -8,17 +8,25 @@ from .models import Attendance, Module, Session, Signup, WaitingList
 class SessionAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "mentor":
+            # Filter users who belong to the "Mentor" group
             mentor_group = Group.objects.filter(name="Mentor").first()
             if mentor_group:
-                kwargs["queryset"] = User.objects.filter(groups=mentor_group).order_by(
+                kwargs["queryset"] = User.objects.filter(groups=mentor_group)
+                # Customize the display label in the dropdown
+                kwargs["queryset"] = kwargs["queryset"].order_by(
                     "first_name", "last_name"
                 )
             else:
-                kwargs["queryset"] = User.objects.none()
+                kwargs["queryset"] = (
+                    User.objects.none()
+                )  # No group found, no users displayed
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-    def label_from_instance(self, obj):
-        return f"{obj.first_name} {obj.last_name}"
+    # Optional: Customize how users are displayed in the dropdown
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(full_name=Concat("first_name", Value(" "), "last_name"))
+        return qs
 
 
 admin.site.register(Attendance)
