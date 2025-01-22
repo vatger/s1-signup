c09bf997f42310c5cfce9e917b3634f832a9e6f4
fsoest <felix@soest-net.de>
2025-01-22 22:15:08 +0100

    Add comment model

--- S1/waitinglists/models.py
+++ S1/waitinglists/models.py
@@ -85,3 +85,13 @@ class QuizCompletion(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.quiz_id.__str__()}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
