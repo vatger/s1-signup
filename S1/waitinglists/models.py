bfe7388b9bbfee19b80df245ee9c3d9e88d0639f
fsoest <felix@soest-net.de>
2025-01-31 19:58:43 +0100

    Make date added editable

--- S1/waitinglists/models.py
+++ S1/waitinglists/models.py
@@ -13,7 +13,7 @@ class Module(models.Model):
 class WaitingList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
-    date_added = models.DateTimeField(auto_now_add=True, editable=True)
+    date_added = models.DateTimeField(default=now, editable=True)
     completed = models.BooleanField(default=False)
     expiry_date = models.DateTimeField(null=True, blank=True)
     date_completed = models.DateTimeField(null=True, blank=True)
