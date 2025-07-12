45d70cb48beac2004eb849622506919ca61017ab
fsoest <felix@soest-net.de>
2025-07-12 19:34:49 +0200

    Collect static in init.sh

--- init.sh
+++ init.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --noinput
 nginx -g "daemon on;"
 
 crontab cron_schedule && crond
-cd S1 && gunicorn S1.wsgi --bind 127.0.0.1:8016
+cd /opt/s1/S1 && gunicorn S1.wsgi --bind 127.0.0.1:8016
