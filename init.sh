7d34657ca7460791e7dc573ec74ab2dead8773a1
fsoest <46381886+fsoest@users.noreply.github.com>
2025-07-23 21:44:47 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ python manage.py collectstatic --noinput
 
 nginx -g "daemon on;"
 
-crontab cron_schedule && crond
+crontab /opt/s1/cron_schedule && crond
 cd /opt/s1/S1 && gunicorn S1.wsgi --bind 127.0.0.1:8016
