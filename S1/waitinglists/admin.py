84d45fa4b189a5e38edd28036188a1f28b078115
soest <felix.soest@vatger.de>
2025-10-29 09:29:29 +0100

    Test

--- S1/waitinglists/admin.py
+++ S1/waitinglists/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
+from django.contrib.admin.models import LogEntry
 
 
 from .models import (
@@ -57,6 +58,23 @@ class SessionAdmin(admin.ModelAdmin):
                     User.objects.none()
                 )  # No group found, no users displayed
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = (
+        "action_time",
+        "user",
+        "content_type",
+        "object_repr",
+        "action_flag",
+        "change_message",
+    )
+    list_filter = ("user", "action_flag")
+    search_fields = ("object_repr", "change_message")
+
+    def has_add_permission(self, request):
+        return False  # Prevent manual addition
 
 
 admin.site.register(Attendance)
--- S1/waitinglists/management/commands/remove_inactive.py
+++ S1/waitinglists/management/commands/remove_inactive.py
@@ -1,7 +1,11 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.admin.models import DELETION
 
 from waitinglists.models import WaitingList
+
+from S1.helpers import log_admin_action
 
 
 class Command(BaseCommand):
@@ -12,4 +16,10 @@ class Command(BaseCommand):
             completed=False, expiry_date__lt=timezone.now()
         )
         for inactive in inactives:
-            print(inactive.user.first_name + " " + inactive.user.last_name)
+            pass # inactive.delete()
+        log_admin_action(
+            User.objects.get(id=1000),
+            inactive,
+            DELETION,
+            f"Deleted {inactive.user.username}, {inactive.module.name}, {inactive.date_added}, {inactive.expiry_date}",
+        )
