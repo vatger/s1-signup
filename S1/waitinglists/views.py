faac7cb415983f6bbecd4cbe14e2258f52c7bbc4
Jacob Koglin <jlkoglin@icloud.com>
2026-08-03 22:03:46 +0200

    Change past session order from newest to latest for better accessibility

--- S1/waitinglists/views.py
+++ S1/waitinglists/views.py
@@ -453,7 +453,7 @@ def total_waiting_list(request, module_id):
 
 @user_passes_test(is_mentor)
 def past_sessions(request):
-    sessions = Session.objects.filter(attendance_done=True).order_by("datetime")
+    sessions = Session.objects.filter(attendance_done=True).order_by("-datetime")
     context = {
         "sessions": sessions,
         "prefer_en": request.user.userdetail.en_preferred,
